3401d6954f6c71efc42699b778818d23f4740538
Mathias Darscht <mdarscht@stud.hs-heilbronn.de>
2022-10-14 08:57:29 +0200

    added a testing function for orientation

--- taesch/lib/pages/view/shopping_list_page.dart
+++ taesch/lib/pages/view/shopping_list_page.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:taesch/pages/view_model/shopping_list_page_vm.dart';
 
 /// this page contains all the shopping list elements
 class ShoppingListPage extends StatefulWidget {
-  const ShoppingListPage({super.key, required this.title});
+  ShoppingListPage({super.key, required this.title});
 
   final String title;
+  final ShoppingListPageVM _vm = ShoppingListPageVM();
 
   @override
   State<ShoppingListPage> createState() => _ShoppingListPageState();
 }
 
 class _ShoppingListPageState extends State<ShoppingListPage> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +24,9 @@ class _ShoppingListPageState extends State<ShoppingListPage> {
         child: Column(),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          widget._vm.test();
+        },
         child: const Icon(Icons.add),
       ),
     );

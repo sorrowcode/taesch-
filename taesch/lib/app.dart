933ed22e5ed02fa3c653cb8da09c95df705f3816
Mathias Darscht <mdarscht@stud.hs-heilbronn.de>
2022-10-14 08:27:03 +0200

    adjusted class

--- taesch/lib/app.dart
+++ taesch/lib/app.dart
@@ -1,29 +1,21 @@
 import 'package:flutter/material.dart';
 
-import 'home_screen.dart';
-import 'main.dart';
+import 'shopping_list_page.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+/// this class is the root element of the widget tree
+///
+/// all configuration happens here
+class App extends StatelessWidget {
+  const App({super.key});
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Taesch!',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const HomeScreen(title: 'Flutter Demo Home Page'),
+      home: const ShoppingListPage(title: 'Shopping List'),
     );
   }
 }

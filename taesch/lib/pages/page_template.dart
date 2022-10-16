c13ac3c15d419b8fa302cf831281fa4d8229adba
Mathias Darscht <mdarscht@stud.hs-heilbronn.de>
2022-10-16 07:37:53 +0200

    created page template
    - created page template with the basic structure of every page
    - added the drawer functionality that every page has a drawer navigation object

---
+++ taesch/lib/pages/page_template.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+import '../model/screen.dart';
+
+/// template page with the needed structure
+abstract class PageTemplate extends StatefulWidget{
+  final String title;
+  const PageTemplate({super.key, required this.title});
+}
+
+abstract class PageTemplateState extends State<PageTemplate> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: body(),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: Text(Screen.shoppingList.text),
+              onTap: () {
+                setState(() {
+                  Navigator.pushNamed(context, Screen.shoppingList.text);
+                });
+              },
+            ),
+            ListTile(
+              title: Text(Screen.nearShops.text),
+              onTap: () {
+                setState(() {
+                  Navigator.pushNamed(context, Screen.nearShops.text);
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget body();
+}

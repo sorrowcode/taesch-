b83d64a394ada25aeb02535c576150ad79b729fc
Mathias Darscht <mdarscht@stud.hs-heilbronn.de>
2022-10-17 16:16:50 +0200

    created main testing file for better organization

--- taesch/test/ui/sidebar_test.dart
+++ taesch/test/ui/sidebar_test.dart
@@ -1,10 +1,14 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:taesch/app.dart';
+import 'package:taesch/model/screen.dart';
+import 'package:taesch/pages/view/shopping_list_page.dart';
 
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const App());
+    ShoppingListPage page = ShoppingListPage(title: Screen.shoppingList.text);
+    expect(find.byWidget(page), findsOneWidget);
   });
 }

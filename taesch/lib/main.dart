3c58de36cadd0d214b889be3614aa4c2aab25ba5
max <maximilianthomasley@gmail.com>
2022-10-18 15:36:04 -0300

    first successful test with geolocator

--- taesch/lib/main.dart
+++ taesch/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 import 'app.dart';
 
 /// entry point
 void main() {
   runApp(const App());
+  Future<Position> position = Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
 }

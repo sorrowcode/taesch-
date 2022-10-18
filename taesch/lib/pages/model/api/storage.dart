8668da4d128663a81d4a2cd86709f55da492b3d8
Köhler, Jonathan <Jonathan.Koehler@adesso.de>
2022-10-18 13:28:34 +0200

    easy Browser locale-storage

---
+++ taesch/lib/pages/model/api/storage.dart
@@ -0,0 +1,13 @@
+// Stores data permanently
+class PersistStorage {
+
+  // retrieves stored data
+  dynamic getRepo() {}
+
+  // adds or overwrites stored data
+  void writeRepo(dynamic data) {}
+
+  // clears stored data
+  void clearRepo() {}
+
+}
---
+++ taesch/lib/pages/model/shoppingitem.dart
@@ -0,0 +1,9 @@
+import 'dart:typed_data';
+
+class ShoppingItem{
+  ShoppingItem(this.title, this.image);
+
+  String title;
+  ByteBuffer image;
+
+}

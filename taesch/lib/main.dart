07541887e4f6c72ca4b0d2fb1218de2fbbb37ca2
max <maximilianthomasley@gmail.com>
2022-10-25 12:43:38 -0300

    successful test http request to overpass API output as JSON

--- taesch/lib/main.dart
+++ taesch/lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:taesch/geo_tester.dart';
+//import 'package:flutter_geo_guesser/api/overpass_api.dart'
 
 import 'app.dart';
 
 /// entry point
 void main() {
   runApp(const App());
-  Future<Position> position = Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+  //Future<LocationPermission> permission = Geolocator.requestPermission();
+  //Future<Position> position = Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);//sdks 26 - 33
+
+  GeoTester geoTester = GeoTester();
+  geoTester.makeHTTPRequest();
 }

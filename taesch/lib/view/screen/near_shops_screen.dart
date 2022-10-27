2149f7ace7b1270db46c3144b13ec5915ec1ff07
max <maximilianthomasley@gmail.com>
2022-10-27 09:07:53 -0300

    shops now are displayed on near_shops_screen

--- taesch/lib/view/screen/near_shops_screen.dart
+++ taesch/lib/view/screen/near_shops_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:taesch/api/map_api_logic/api_querier.dart';
+
+import '../../model/map_spot.dart';
 
 /// shows the shops which are near to the own location
 class NearShopsScreen extends StatefulWidget {
@@ -10,8 +13,65 @@ class NearShopsScreen extends StatefulWidget {
 }
 
 class _NearShopsScreenState extends State<NearShopsScreen> {
+
+    APIQuerier apiQuerier = APIQuerier();
+    bool _toggler = false;
+
+    List<Widget> createSpotWidgets(List<MapSpot> list){
+        var elements = <Widget>[];
+        for (int i=0; i<list.length; i++){
+            var entry = list[i];
+            var cont = Container(
+                child: Column(
+                    children: [
+                        Text(entry.name,
+                        style: const TextStyle(fontWeight: FontWeight.bold)),
+                        Text(entry.address),
+                        Text("   ")
+                    ],
+                ),
+            );
+            elements.add(cont);
+        }
+        return elements;
+    }
+
   @override
-  Widget build(BuildContext context) {
-    return const Center();
+  Widget build(BuildContext context){
+    //return const Center();
+      apiQuerier.makeHTTPRequest();
+      var mapdata = apiQuerier.extractJSONData();
+      var scrollWidgetList = createSpotWidgets(mapdata);
+      print("SUCCESS");
+
+    return Scaffold(
+      body: Column(
+          children: [
+              Container(
+                  height: 100,
+                  //color: _toggleColor? Colors.purple : Colors.blue,
+                  child: Column(
+                      children: [
+                          Text("Area: Heilbronn",
+                              style: TextStyle(fontSize: 25)),
+                          TextButton(
+                              child: Text("Search"),
+                              onPressed: (){setState(() {
+                                  _toggler = !_toggler;
+                              });},
+                          )
+                      ],
+                  ),
+              ),
+              Expanded(
+                  child: SingleChildScrollView(
+                      child: Column(
+                          children: _toggler? scrollWidgetList : scrollWidgetList,
+                      ),
+                  )
+              )
+          ],
+      )
+    );
   }
 }

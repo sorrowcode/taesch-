90fa67fcd007ed6fbd993616885c741d42d9d309
Köhler, Jonathan <Jonathan.Koehler@adesso.de>
2022-11-06 12:38:11 +0100

    dark mode shared prefs

--- taesch/lib/api/repository.dart
+++ taesch/lib/api/repository.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:taesch/model/shop.dart';
 import 'package:taesch/model/shopping_list_item.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Repository {
-  bool isDarkModeEnabled = false;
+  bool _isDarkModeEnabled = false;
   final List<ShoppingListItem> _shoppingListItems = [];
   ValueNotifier<int> shoppingListSize = ValueNotifier(0);
   List<Shop> shopsCache = [];
@@ -15,7 +16,15 @@ class Repository {
     return _singleton;
   }
 
-  Repository._internal();
+  set isDarkModeEnabled(bool enabled) {
+    _isDarkModeEnabled = enabled;
+    SharedPreferences.getInstance().then((prefs) => prefs.setBool('dark_mode_enabled', enabled));
+  }
+  bool get isDarkModeEnabled => _isDarkModeEnabled;
+
+  Repository._internal(){
+    SharedPreferences.getInstance().then((prefs) => _isDarkModeEnabled = prefs.containsKey('dark_mode_enabled')?prefs.getBool('dark_mode_enabled')!:false);
+  }
 
   List<ShoppingListItem> get shoppingListItems => _shoppingListItems;
 }

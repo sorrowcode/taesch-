9f2ba0365418a3d1bd07cd49d81d8d0502273e75
max <maximilianthomasley@gmail.com>
2022-11-06 10:06:36 -0300

    sample code for accessing location with Geolocator. Future works, but method needs further refinement

--- taesch/lib/main.dart
+++ taesch/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:taesch/api/map_api_logic/geolocation_tools.dart';
 
 import 'app.dart';
 
@@ -7,7 +9,13 @@ void main() {
   //Future<LocationPermission> permission = Geolocator.requestPermission();
   //Future<Position> position = Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);//sdks 26 - 33
 
+  //Future<bool> permission = GeolocationTools.handleLocationPermission();
+
   //APIQuerier apiQuerier = APIQuerier();
   //apiQuerier.makeHTTPRequest();
   runApp(App());
+
+  // the following can only be called after WidgetsFlutterBinding.ensureInitialized() has been called
+  print("fetch Position");
+  GeolocationTools.getCurrentPosition();
 }
--- taesch/lib/utils/my_tools.dart
+++ taesch/lib/utils/my_tools.dart
@@ -18,4 +18,5 @@ class MyTools {
     }
     return result;
   }
+
 }

a6c9ecd3cf5dc68e1c299542fbfa28d8b288b609
max <maximilianthomasley@gmail.com>
2022-11-06 23:49:04 -0300

    created a screen for shops on  a map

---
+++ taesch/lib/view/screen/shops_map_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:taesch/view_model/screen/shops_map_screen_vm.dart';
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+class ShopsMapScreen extends StatefulWidget{
+    final _vm = ShopsMapScreenVM();
+
+    ShopsMapScreen({super.key});
+
+    @override
+    State<StatefulWidget> createState() => _ShopsMapScreenState();
+}
+
+class _ShopsMapScreenState extends State<ShopsMapScreen>{
+    @override
+    Widget build(BuildContext context) {
+
+        // taken from the example code of https://github.com/fleaflet/flutter_map/blob/master/example/lib/pages/moving_markers.dart
+        return  Scaffold(
+            body: Center(
+                child: SizedBox(
+                    width: 200,
+                    height: 200,
+                    child: FlutterMap(
+                        options: MapOptions(
+                            center: LatLng(45.5231, -122.6765),
+                            zoom: 13,
+                        ),
+                        children: [
+                            TileLayer(
+                                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                                userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+                            ),
+                        ],
+                    ),
+                ),
+            ),
+        );
+    }
+}

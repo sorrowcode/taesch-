f8af61ee826db738b2a06c22cbf3c46a20110e1c
Mathias Darscht <mdarscht@stud.hs-heilbronn.de>
2022-11-14 06:58:41 +0100

    added static logger for only one instance

--- taesch/lib/middleware/log/logger_wrapper.dart
+++ taesch/lib/middleware/log/logger_wrapper.dart
@@ -2,24 +2,22 @@ import 'package:logger/logger.dart';
 import 'package:taesch/middleware/log/log_level.dart';
 
 class LoggerWrapper {
-  late Logger _logger;
+  static Logger? _logger;
 
   LoggerWrapper() {
-    _logger = Logger(
-      printer: PrettyPrinter(),
-    );
+    _logger ??= Logger(printer: PrettyPrinter());
   }
 
   void log({required LogLevel level, required String message}) {
     switch(level) {
       case LogLevel.debug:
-        _logger.d(message);
+        _logger!.d(message);
         break;
       case LogLevel.info:
-        _logger.i(message);
+        _logger!.i(message);
         break;
       case LogLevel.error:
-        _logger.e(message);
+        _logger!.e(message);
         break;
     }
   }

927dfb542b0a0b1115d10f7e93801ad503915a29
Köhler, Jonathan <Jonathan.Koehler@adesso.de>
2022-11-15 18:29:03 +0100

    StorageTags class

---
+++ taesch/lib/api/storage_tags.dart
@@ -0,0 +1,28 @@
+
+import 'package:taesch/api/storage.dart';
+
+import '../model/tag.dart';
+
+class StorageTags implements PersistStorage<Tag>{
+  @override
+  void delete(Tag data) {
+    // TODO: implement delete
+  }
+
+  @override
+  void insert(Tag data) {
+    // TODO: implement insert
+  }
+
+  @override
+  read(Map<String, dynamic> filter) {
+    // TODO: implement read
+    throw UnimplementedError();
+  }
+
+  @override
+  void update(Tag data) {
+    // TODO: implement update
+  }
+  
+}

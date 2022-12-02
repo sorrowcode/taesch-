314c19923f92933cc54ae094eb500c527043671b
sorrowcode <sorrow-code@outlook.com>
2022-12-02 08:49:26 +0100

    refactored shops_tile.dart regarding vm

--- taesch/lib/view/custom_widget/shops_tile.dart
+++ taesch/lib/view/custom_widget/shops_tile.dart
@@ -3,11 +3,13 @@ import 'package:taesch/middleware/log/logger_wrapper.dart';
 import 'package:taesch/view_model/custom_widget/shops_tile_vm.dart';
 
 class ShopsTile extends StatefulWidget {
-  final ShopsTileVM _vm = ShopsTileVM();
 
-  ShopsTile({super.key, required String title, required String address}) {
-    _vm.title = title;
-    _vm.address = address;
+  late final String _title;
+  late final String _address;
+
+  ShopsTile({super.key, required String title, required String address}){
+    _title = title;
+    _address = address;
   }
 
   @override
@@ -16,6 +18,11 @@ class ShopsTile extends StatefulWidget {
 
 class _ShopsTileState extends State<ShopsTile> {
   LoggerWrapper logger = LoggerWrapper();
+  late final ShopsTileVM _vm;
+
+  _ShopsTileState() {
+    _vm = ShopsTileVM.withData(title: widget._title, address: widget._address);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +33,7 @@ class _ShopsTileState extends State<ShopsTile> {
                 borderRadius: BorderRadius.circular(30.0)),
             tileColor: Theme.of(context).listTileTheme.tileColor,
             leading: const Icon(Icons.shopping_cart),
-            title: Text(widget._vm.title),
-            subtitle: Text(widget._vm.address)));
+            title: Text(_vm.title),
+            subtitle: Text(_vm.address)));
   }
 }
--- taesch/lib/view_model/custom_widget/shops_tile_vm.dart
+++ taesch/lib/view_model/custom_widget/shops_tile_vm.dart
@@ -2,7 +2,5 @@ class ShopsTileVM {
   late String title;
   late String address;
 
-  ShopsTileVM();
-
   ShopsTileVM.withData({required this.title, required this.address});
 }

df2436cc6b76b921da608d9f453049045c73d7c4
max <maximilianthomasley@gmail.com>
2022-12-06 21:03:12 -0300

    longpress auf Icon mit pop up

--- taesch/lib/view/screen/shops_map_screen.dart
+++ taesch/lib/view/screen/shops_map_screen.dart
@@ -48,6 +48,82 @@ class _ShopsMapScreenState extends State<ShopsMapScreen> {
                 _onTapId = markerID;
               });
             },
+
+            onLongPress: (){
+              showDialog(
+                  context: ctx,
+                  builder: (ctx) => AlertDialog(
+                    title: Text(mapSpot.name),
+                    content: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          RichText(
+                            text: const TextSpan(
+                                  style: TextStyle(color: Colors.black), //apply style to all
+                                  children: [
+                                    TextSpan(text: 'Street:', style: TextStyle(fontWeight: FontWeight.bold)),
+                                    TextSpan(text: ' some street name'),
+                                    TextSpan(text: "\n")
+                                  ]
+                              )
+                          ),
+                          RichText(
+                              text: const TextSpan(
+                                  style: TextStyle(color: Colors.black), //apply style to all
+                                  children: [
+                                    TextSpan(text: 'Number:', style: TextStyle(fontWeight: FontWeight.bold)),
+                                    TextSpan(text: ' 17'),
+                                    TextSpan(text: "\n")
+                                  ]
+                              )
+                          ),
+                          RichText(
+                              text: const TextSpan(
+                                  style: TextStyle(color: Colors.black), //apply style to all
+                                  children: [
+                                    TextSpan(text: 'Distance to you:', style: TextStyle(fontWeight: FontWeight.bold)),
+                                    TextSpan(text: ' 44m'),
+                                    TextSpan(text: "\n")
+                                  ]
+                              )
+                          ),
+                          RichText(
+                              text: const TextSpan(
+                                  style: TextStyle(color: Colors.black), //apply style to all
+                                  children: [
+                                    TextSpan(text: 'Öffnungszeiten:', style: TextStyle(fontWeight: FontWeight.bold)),
+                                    TextSpan(text: ' 8:00 - 19:30 Uhr'),
+                                    TextSpan(text: "\n")
+                                  ]
+                              )
+                          ),
+                          RichText(
+                              text: const TextSpan(
+                                  style: TextStyle(color: Colors.black), //apply style to all
+                                  children: [
+                                    TextSpan(text: 'Toilette:', style: TextStyle(fontWeight: FontWeight.bold)),
+                                    TextSpan(text: ' Nein'),
+                                    TextSpan(text: "\n")
+                                  ]
+                              )
+                          ),
+                          RichText(
+                              text: const TextSpan(
+                                  style: TextStyle(color: Colors.black), //apply style to all
+                                  children: [
+                                    TextSpan(text: 'Parkplatz:', style: TextStyle(fontWeight: FontWeight.bold)),
+                                    TextSpan(text: ' Ja'),
+                                    TextSpan(text: "\n")
+                                  ]
+                              )
+                          ),
+                        ],
+                      ),
+                    ),
+
+                  )
+              );
+            },
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               //verticalDirection: VerticalDirection.up,

d7acaddf70c72b977f05d1f95b334dae4da672b9
sorrowcode <sorrow-code@outlook.com>
2022-12-12 10:09:18 +0100

    implemented the init method for the firebase wrapper

--- taesch/lib/api/implementation/firebase_wrapper.dart
+++ taesch/lib/api/implementation/firebase_wrapper.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:taesch/api/actions/firebase_actions.dart';
+
+import 'firebase_options.dart';
 
 class FirebaseWrapper implements FirebaseActions {
   @override
-  void init() {
-    // TODO: implement init
+  void init() async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
   }
 }

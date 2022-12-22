f97fb332e5b61c6b7a29e682df862b3f31c2f71b
sorrowcode <sorrow-code@outlook.com>
2022-12-22 11:07:36 +0100

    created shell for unit testing

--- taesch/test/unit_test_main.dart
+++ taesch/test/unit_test_main.dart
@@ -1,3 +1,20 @@
-void main() async {
+import 'package:flutter_test/flutter_test.dart';
 
+void main() async {
+  group("validation", () {
+    /* todo:
+    - email validation
+    - password validation
+    - username validation ?
+    - same password validation
+    */
+  });
+
+  group("actions", () {
+    /* todo:
+      - sql actions
+      - firebase actions
+      - osm actions
+     */
+  });
 }

6f1ae4d4aff60bdf4e8092a252cbf8c506c68ea1
sorrowcode <sorrow-code@outlook.com>
2023-01-02 16:38:58 +0100

    tested email validation

--- taesch/test/unit_test.dart
+++ taesch/test/unit_test.dart
@@ -1,4 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:taesch/model/error_case.dart';
+import 'package:taesch/view_model/page/login_page_vm.dart';
+import 'package:taesch/view_model/page/starting_page_vm.dart';
 
 void main() async {
   group("validation", () {
@@ -10,7 +13,24 @@ void main() async {
     */
 
     group("email", () {
+      StartingPageVM vm = LoginPageVM();
 
+      test("testing null or empty values", () {
+        var result = vm.validateEMail(null);
+        expect(result, ErrorCase.noEmail);
+        result = vm.validateEMail("");
+        expect(result, ErrorCase.noEmail);
+      });
+
+      test("testing wrong content", () {
+        var result = vm.validateEMail("value");
+        expect(result, ErrorCase.invalidEmail);
+      });
+
+      test("testing valid value", () {
+        var result = vm.validateEMail("johndoe@mail.com");
+        expect(result, null);
+      });
     });
 
     group("password", () {
